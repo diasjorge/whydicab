3fe2efe27e2c35b2a88510c382033fa3cf08d808
Jorge Dias <pana@qvitta.net>
2008-12-13 19:10:11 +0100

    Preventing possible SQL Injection in search

--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -3,11 +3,16 @@ class Search < Application
 
   def index
     @query = Merb::Parse.unescape(params[:q]||"")
-    pagination = { :per_page => 10, :page => params[:page] }
-    @results = [].paginate(pagination)
-    @empty_query = (@query.nil? || @query.strip.empty?)
-    @results = Article.paginate({ :conditions => ["body like '%#{@query}%' OR title like '%#{@query}%'"],
-                                  :order => [:created_at.desc]}.merge(pagination)) unless @empty_query
+    if @query.nil? || @query.strip.empty?
+      @results = []
+    else
+      @results = Article.paginate(:conditions => ["(body like ? OR title like ?)",
+                                                  "%#{@query}%","%#{@query}%"],
+                                  :published => true,
+                                  :order => [:created_at.desc],
+                                  :per_page => 10,
+                                  :page => params[:page])
+    end
     display @results
   end
 
