703f7199dabe8aa950420628e81de6d0b217bd56
Jorge Dias <jorge@mrdias.com>
2009-02-19 01:42:51 +0100

    Fixed bug in sidebar links. Site links in settings

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,17 +1,17 @@
 module Merb
   module GlobalHelpers
-    SITES = Mash.new("Qvitta" => "http://qvitta.net",
-                     "Merb" => "http://merbivore.com",
-                     "DataMapper" => "http://datamapper.org",
-                     "Least Significant Bit" => "http://least-significant-bit.com")
-
     # helpers defined here available to all views.
+
     def site_links
-      SITES.map{|x| "<li>#{link_to x[0], x[1]}</li>"}.join("")
+      Settings.site_links || []
+    end
+
+    def sidebar_links
+      site_links.map{|x| "<li>#{link_to x[0], x[1]}</li>"}.join("")
     end
 
     def navigation_links
-      SITES.map{|x| link_to x[0], x[1]}.join(" | ")
+      site_links.map{|x| link_to x[0], x[1]}.join(" | ")
     end
 
   end
